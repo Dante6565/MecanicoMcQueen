1655160e243323a14ae37ee1ea80213d9bb9bd77
Dante Reyes <86571643+Dante6565@users.noreply.github.com>
2022-04-28 13:13:59 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const form = document.querySelector("form"),
+        nextBtn = form.querySelector(".nextBtn"),
+        backBtn = form.querySelector(".backBtn"),
+        allInput = form.querySelectorAll(".first input");
+
+
+nextBtn.addEventListener("click", ()=> {
+    allInput.forEach(input => {
+        if(input.value != ""){
+            form.classList.add('secActive');
+        }else{
+            form.classList.remove('secActive');
+        }
+    })
+})
+backBtn.addEventListener("click", () => form.classList.remove('secActive'));
+
+function buscar()
+{
+    var valor_a_buscar=document.getElementById('barrabuscadora').value;
+    valor_a_buscar = valor_a_buscar.toUpperCase();
+    var lista_palabras= ['JAVIER GONZALES','RODRIGO SANCHEZ','GONZALO PEÑALOZA']
+    if(lista_palabras.includes(valor_a_buscar))
+    {
+        if(valor_a_buscar==lista_palabras[0])
+        {
+            window.open("JavierGonzales.html")
+        }
+        else if(valor_a_buscar==lista_palabras[1])
+        {
+            window.open("RodrigoSanchez.html")
+        }
+        else if(valor_a_buscar==lista_palabras[2])
+        {       
+            window.open("GonzaloPeñaloza.html")
+        }
+    }
+    else
+    {
+        alert("No se encontro la palabra o oración buscada");
+    }
+}
